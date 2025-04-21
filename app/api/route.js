2f3f500fe19f182663608543866a788640b15bd5
51pgwamuyu <elizabethwakarima51@gmail.com>
2025-04-21 07:37:56 +0300

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAi = new GoogleGenerativeAI(process.env.API_KEY);
 export async function POST(request) {
   const data = await request.json();
-  const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash"  });
+  const model = genAi.getGenerativeModel({  model: "gemini-2.0-flash", });
   const prompt = `You are Dr. Love, answer ${data.message} question in form of love like find connection of love in users questions and anawer it correctly`
   const result = await model.generateContent(prompt);
   const response = result.response;
@@ -11,3 +11,4 @@ export async function POST(request) {
 
   return Response.json(text);
 }
+
